63998e5c1a209842f698ead872203de12ee3fe9c
dextrenvy <31600348+dextrenvy@users.noreply.github.com>
2020-04-08 21:27:48 +0300

    Update click.py

--- click.py
+++ click.py
@@ -1,15 +1,34 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.showbase import DirectObject
+from direct.task import Task
 from panda3d.core import LineSegs, NodePath, Point3D
 from math import cos, sin, pi
 from numpy import append
+
+
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.taskMgr.add(self.get_coords, "get_coords")
+
+    def get_coords(self, task):
+        global points
+        points = []
+        if self.mouseWatcherNode.hasMouse():
+            x = self.mouseWatcherNode.getMouseX()
+            y = self.mouseWatcherNode.getMouseY()
+            points = append(x,y)
+        print(points)
+        return Task.cont
+
 
 class Hello(DirectObject.DirectObject):
     def __init__(self):
         global points
         self.points = []
         self.accept("mouse1", self.get_coords)
-        self.accept("mouse3", self.set_coords)
+        #self.accept("mouse3", self.set_coords)
 
 
 
@@ -31,6 +50,6 @@ class Hello(DirectObject.DirectObject):
 
 
 
-base = ShowBase()
-test = Hello()
-base.run()
+app = MyApp()
+h = Hello()
+app.run()
