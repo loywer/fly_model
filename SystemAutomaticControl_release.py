06fcb04c4658e99174e4031c50c74c7bf0d9e21f
ZheleznovIlia <62719911+ZheleznovIlia@users.noreply.github.com>
2020-04-08 22:00:14 +0300

    Обновление управления

--- SystemAutomaticControl_release.py
+++ SystemAutomaticControl_release.py
@@ -12,55 +12,40 @@ class Control:
 
     # Функция получения положения руля высоты и текущей перегрузки 
     #   ny_spec - заданное значение перегрузки 
-    def get_elev_and_ny_new (self, ny_spec) :
+    def get_elev_and_ny_new (self, ny_spec, ny_now) :
              
-        self.ny_now = 0 # начальное значение перегрузки
-        self.elev_new = 0 # начальное положение угла руля высоты
+        #self.ny_now = 0 # начальное значение перегрузки
+        #self.elev_new = 0 # начальное положение угла руля высоты
         
-        self.t = 0
-
         #while ((abs(self.ny_now - self.ny_spec) > eps_ny) and (t <= 10)) :
-        while (self.t <= 10) :
+     
+        #self.ny_now = get_ny(self.elev_new) / ACCELERATION_OF_GRAVITY # получение значения перегрузки в данный момент времени
+        self.elev_new = self.elev_new + k_elev * (ny_spec - ny_now) # получение отклонения рулей высоты
+        #elev_new =  0.1 * (ny_spec - ny_now) # получение отклонения рулей высоты
+
+        if (self.elev_new * 180.0/np.pi >= 26) :
+             self.elev_new = 26/180.0*np.pi
+
+        if (self.elev_new * 180.0/np.pi <= -28) :
+            self.elev_new = -28/180.0*np.pi
         
-            self.ny_now = get_ny(self.elev_new) / ACCELERATION_OF_GRAVITY # получение значения перегрузки в данный момент времени
-            self.elev_new = self.elev_new + k_elev * (ny_spec - self.ny_now) # получение отклонения рулей высоты
-            #elev_new =  0.1 * (ny_spec - ny_now) # получение отклонения рулей высоты
-
-            if (self.elev_new * 180.0/np.pi >= 26) :
-                self.elev_new = 26/180.0*np.pi
-
-            if (self.elev_new * 180.0/np.pi <= -28) :
-                self.elev_new = -28/180.0*np.pi
-        
-
-            self.t = self.t + dt
             
         return self.elev_new
 
     # Функция получения положения элерона и крена самолета 
     #   gamma_spec - заданное значение крена самолета (желаемое)
-    def get_GammaAngle_and_eleron_now (self, gamma_spec) :
-        
-        self.eleron_now = 0.0 # начальное положение элерона
-        self.gamma_now = 0 # начальный угол крена самолета
-        
-        
-        self.t = 0
+    def get_GammaAngle_and_eleron_now (self, gamma_spec, gamma_now, w0) :
+           
+        #self.gamma_now, self.w0 = get_gamma(self.eleron_now)
 
-        #while ((abs(gamma_spec - gamma_now) > 0.01) and (t <= 20)) :
-        while  (self.t <= 20):
-            
-            self.gamma_now, self.w0 = get_gamma(self.eleron_now)
+        self.eleron_now = k_eleron * (gamma_spec - gamma_now) - 3*w0
 
-            self.eleron_now = k_eleron*(gamma_spec - self.gamma_now) - 3*self.w0
+        if (self.eleron_now * 180.0/np.pi >= 20) :
+            self.eleron_now = 20/180.0*np.pi
 
-            if (self.eleron_now * 180.0/np.pi >= 20) :
-                self.eleron_now = 20/180.0*np.pi
+        if (self.eleron_now * 180.0/np.pi <= -15) :
+            self.eleron_now = -15/180.0*np.pi
 
-            if (self.eleron_now * 180.0/np.pi <= -15) :
-                self.eleron_now = -15/180.0*np.pi
-
-            self.t = self.t + dt
         return self.eleron_now
 
 
