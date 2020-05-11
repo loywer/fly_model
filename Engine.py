c81209e6810181ac8acaba448d70ac19af86ba73
ZheleznovIlia <62719911+ZheleznovIlia@users.noreply.github.com>
2020-05-11 22:31:58 +0300

    Update Engine.py

--- Engine.py
+++ Engine.py
@@ -71,7 +71,7 @@ class Engine(object):
 
     # Функция вывода значений
     def Get_data(self):
-        sp_omega = self.spec_omega(self.spec_speed, self.current_speed)
+        sp_omega = self.Get_mode(1)
         thrust = self.get_tractive_power(sp_omega, self.current_speed)
         moment = self.torque(sp_omega, self.spec_speed)
         return thrust, moment
